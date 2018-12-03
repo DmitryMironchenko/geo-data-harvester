ca1aa1728a48f1012ccca76a6a4eec0b3d5453b6
ramanbirulia <raman.birulia@gmail.com>
2018-12-03 17:36:01 +0300

    Dairy farming initial commit

--- src/app/operator-dashboard/dash.component.ts
+++ src/app/operator-dashboard/dash.component.ts
@@ -9,10 +9,10 @@ import { ToastrService } from 'ngx-toastr';
 
 export class DashComponent implements OnInit {
   title = 'Fortescue Metals Group Iron Ore Mine';
-  lat = -22.3269188;
-  lng = 119.4059056;
+  lat = 50.161785;
+  lng = -120.136131;
   height = 643;
-  zoom = 15;
+  zoom = 20;
   hash = null;
   Math = Math;
 
@@ -22,7 +22,7 @@ export class DashComponent implements OnInit {
     image: 'assets/car1.jpeg',
   };*/
   dangerZone = null;
-  markers: Marker[] = [];
+  markers: CowMarker[] = [];
   markerHash = {};
   toastHash = {};
   forceOpenedMarkerId = null;
@@ -34,22 +34,18 @@ export class DashComponent implements OnInit {
   ngOnInit() {
     this.onResize();
 
-    const source = new EventSource('http://10.0.0.134:3001/events/');
+    const source = new EventSource('http://localhost:3001/events/');
     source.onmessage = (e) => {
       this.hash = Math.round(Math.random() * 1e5);
       const data = JSON.parse(e.data);
       data.forEach(d => {
-        if (d.type === 'car breakdown'
-          || d.type === 'driver blood pressure is not normal'
-          || d.type === 'driver got tired'
-          || d.type === 'unit violates zone restrictions') {
-          // this.errors[d.id] = d;
-
-          if (d.level === 'ERROR' || d.driverStatus === 'CRITICAL') {
+        if (!d) return;
+        if (d.showAlert) {
+          if (d.status === 'CRITICAL') {
             const toast = this.toastr
-              .error(`(#${d.carId}): ${d.description}`, d.title, {
+              .error(`${d.description}`, d.title, {
                 // progressBar: true,
-                timeOut: 2e3,
+                timeOut: 5e3,
                 closeButton: true,
                 tapToDismiss: false,
               });
@@ -60,15 +56,16 @@ export class DashComponent implements OnInit {
             if (!this.toastHash[d.id]) {
               const subscription = toast.onTap.subscribe((foo: any) => {
                 // console.log('[Info] toast tapped', d);
-                this.forceOpenedMarkerId = d.carId;
+                this.forceOpenedMarkerId = d.cowId;
               });
               this.toastHash[d.id] = subscription;
             }
-          } else if (d.level === 'WARNING') {
+          }
+          else if (d.status === 'WARNING') {
             const toast = this.toastr
-              .warning(`(#${d.carId}): ${d.description}`, d.title, {
+              .warning(`${d.description}`, d.title, {
                 // progressBar: true,
-                timeOut: 2e3,
+                timeOut: 5e3,
                 closeButton: true,
                 tapToDismiss: false,
               });
@@ -80,7 +77,7 @@ export class DashComponent implements OnInit {
             if (!this.toastHash[d.id]) {
               const subscription = toast.onTap.subscribe((foo: any) => {
                 // console.log('[Info] toast tapped', d);
-                this.forceOpenedMarkerId = d.carId;
+                this.forceOpenedMarkerId = d.cowId;
               });
               this.toastHash[d.id] = subscription;
             }
@@ -88,25 +85,18 @@ export class DashComponent implements OnInit {
           return;
         }
 
-        if (d.type === 'car' || d.type === 'excavator' || d.type === 'pedestrian') {
+        if (d.type === 'car'
+          || d.type === 'excavator'
+          || d.type === 'pedestrian'
+          || d.type === 'waterpoint'
+          || d.type === 'cow') {
           const marker = this.markerHash[d.id];
           const _marker = marker && this.markers[marker.index];
 
           if (marker) {
             // console.log('[INFO] Marker position upd', marker);
-            marker.lat = _marker.lat = d.lat;
-            marker.lng = _marker.lng = d.lng;
-            marker.fuelAmount = _marker.fuelAmount = d.fuelAmount;
-            marker.coolantTemp = _marker.coolantTemp = d.coolantTemp;
-            marker.powerOutput = _marker.powerOutput = d.powerOutput;
-            marker.affects = _marker.affects = d.affects || [];
-            marker.driverTemperature = _marker.driverTemperature = d.driverTemperature;
-            marker.driverHeartRate = _marker.driverHeartRate = d.driverHeartRate;
-            marker.driverBloodPressure1 = _marker.driverBloodPressure1 = d.driverBloodPressure1;
-            marker.driverBloodPressure2 = _marker.driverBloodPressure2 = d.driverBloodPressure2;
-            marker.driverShiftLength = _marker.driverShiftLength = d.driverShiftLength;
-            marker.driverStatus = _marker.driverStatus = d.driverStatus;
-            marker.carStatus = _marker.carStatus = d.carStatus;
+            Object.assign(_marker, d);
+            Object.assign(marker, d);
           } else {
             const length = this.markers.push({
               // ...this.defaultCar,
@@ -161,31 +151,34 @@ export class DashComponent implements OnInit {
 }
 
 // just an interface for type safety.
-interface Marker {
-  id: string;
+interface CowMarker {
+  id: number;
+  type: string;
+  status: string;
+  statusLevel: number;
+  affects: any;
+  showAlert: Boolean;
+  fullName: string;
+  photo: string;
+  age: number;
+  temperature: number;
+  heartRate: number;
+  systolicBloodPressure: number;
+  diastolicBloodPressure: number;
   lat: number;
   lng: number;
-  label?: string;
+}
+
+interface WaterPointMarker {
+  id: number;
   type: string;
-  image: string;
-  draggable: boolean;
-  affects: any[];
-  driver: {
-    fullName: string,
-    photo: string,
-  };
-  iconUrl: string;
-  driverStatus: string;
-  carStatus: string;
-  powerOutput: number;
-  coolantTemp: number;
-  fuelAmount: number;
-  fuelTankCapacity: number;
-  driverTemperature: number;
-  driverHeartRate: number;
-  driverBloodPressure1: number;
-  driverBloodPressure2: number;
-  driverShiftLength: number;
+  status: string;
+  statusLevel: number;
+  affects: any;
+  showAlert: Boolean;
+  photo: string;
+  lat: number;
+  lng: number;
 }
 
 interface Coordinate {
